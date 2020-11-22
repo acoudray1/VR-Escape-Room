cc925ddada1414d67875b1e82e0bbc4b14ffc929
axelc <axelcoudray1@gmail.com>
2020-11-22 14:48:39 +0100

    Auto stash before revert of "cabinet grab interact wip"

---
+++ Assets/ResizeCollider.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResizeCollider : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        BoxCollider boxCollider = gameObject.AddComponent<BoxCollider>();
+        MeshRenderer renderer = gameObject.GetComponent<MeshRenderer>();
+        boxCollider.size = renderer.bounds.size;
+        boxCollider.center = renderer.bounds.center;
+    }
+}
