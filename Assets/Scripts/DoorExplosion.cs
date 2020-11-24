9fbd42b821892d2adf8b677dcaa59138a3e5a667
axelc <axelcoudray1@gmail.com>
2020-11-24 10:43:53 +0100

    explosions and escape impl wip

---
+++ Assets/Scripts/DoorExplosion.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorExplosion : MonoBehaviour
+{
+    [Header("Unity Setup")]
+    public ParticleSystem explosionParticles;
+
+    private void onCollisionEnter(Collision collision) {
+        if (collision.collider.CompareTag("Bomb")) {
+            this.Destroy();
+        }
+    }
+
+    private void Destroy() {
+        Instantiate(this.explosionParticles, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+}
